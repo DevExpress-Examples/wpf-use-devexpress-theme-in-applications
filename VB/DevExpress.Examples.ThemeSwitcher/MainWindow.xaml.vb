c154cc9c37f9dc807fc591bac3a8716d5317e209
DevExpressExampleBot <fakeEmail>
2022-08-09 16:35:05 +0400

    Source auto update [skip ci]

--- VB/DevExpress.Examples.ThemeSwitcher/MainWindow.xaml.vb
+++ VB/DevExpress.Examples.ThemeSwitcher/MainWindow.xaml.vb
@@ -1,62 +1,48 @@
-﻿Imports Microsoft.VisualBasic
 Imports DevExpress.Xpf.Core
-Imports DevExpress.Xpf.Utils.Themes
 Imports System
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace DevExpress.Examples.ThemeSwitcher
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits DXWindow
 
-		Public Sub New()
-			InitializeComponent()
-			Me.dxGrid.ItemsSource = Me.Tasks
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits DXWindow
 
-		#Region "Data"
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.dxGrid.ItemsSource = Tasks
+        End Sub
 
-		Protected _Tasks As List(Of Task)
+#Region "Data"
+        Protected _Tasks As List(Of Task)
 
-		Public ReadOnly Property Tasks() As List(Of Task)
-			Get
-				If Me._Tasks Is Nothing Then
-					Me._Tasks = New List(Of Task)()
-					Dim r As New Random()
-					Dim i As Integer = 0
-					Do While i < 100
-						Me._Tasks.Add(New Task() With {.Name = String.Format("Task {0}", i), .Priority = If(r.Next(2) > 0, Priority.Important, Priority.NotImportant), .StartDate = DateTime.Today.AddDays(-r.Next(20)), .FinishDate = DateTime.Today.AddDays(r.Next(20)), .IsComplete = r.Next(2) > 0})
-						i += 1
-					Loop
-				End If
+        Public ReadOnly Property Tasks As List(Of Task)
+            Get
+                If _Tasks Is Nothing Then
+                    _Tasks = New List(Of Task)()
+                    Dim r As Random = New Random()
+                    Dim i As Integer = 0
+                    While i < 100
+                        _Tasks.Add(New Task() With {.Name = String.Format("Task {0}", i), .Priority = If(r.Next(2) > 0, Priority.Important, Priority.NotImportant), .StartDate = Date.Today.AddDays(-r.Next(20)), .FinishDate = Date.Today.AddDays(r.Next(20)), .IsComplete = r.Next(2) > 0})
+                        i += 1
+                    End While
+                End If
 
-				Return Me._Tasks
-			End Get
-		End Property
+                Return _Tasks
+            End Get
+        End Property
 
-		#End Region ' Data
+#End Region  ' Data
+        Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            ThemeManager.ApplicationThemeName = Theme.DefaultThemeName
+        End Sub
 
-		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			ThemeManager.ApplicationThemeName = Theme.DefaultThemeName
-		End Sub
-
-		Private Sub cbTheme_EditValueChanged(ByVal sender As Object, ByVal e As Xpf.Editors.EditValueChangedEventArgs)
-			ThemeManager.ApplicationThemeName = (TryCast(Me.cbTheme.SelectedItem, Theme)).Name
-		End Sub
-
-	End Class
+        Private Sub cbTheme_EditValueChanged(ByVal sender As Object, ByVal e As Xpf.Editors.EditValueChangedEventArgs)
+            ThemeManager.ApplicationThemeName = TryCast(Me.cbTheme.SelectedItem, Theme).Name
+        End Sub
+    End Class
 End Namespace
--- VB/DevExpress.Examples.ThemeSwitcher/Task.vb
+++ VB/DevExpress.Examples.ThemeSwitcher/Task.vb
@@ -1,66 +1,20 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
-
 Namespace DevExpress.Examples.ThemeSwitcher
 
-	Public Enum Priority
-		Important
-		NotImportant
-	End Enum
+    Public Enum Priority
+        Important
+        NotImportant
+    End Enum
 
-	Public Class Task
-		Private privateName As String
-		Public Property Name() As String
-			Get
-				Return privateName
-			End Get
-			Set(ByVal value As String)
-				privateName = value
-			End Set
-		End Property
+    Public Class Task
 
-		Private privateStartDate As DateTime
-		Public Property StartDate() As DateTime
-			Get
-				Return privateStartDate
-			End Get
-			Set(ByVal value As DateTime)
-				privateStartDate = value
-			End Set
-		End Property
+        Public Property Name As String
 
-		Private privateFinishDate As DateTime
-		Public Property FinishDate() As DateTime
-			Get
-				Return privateFinishDate
-			End Get
-			Set(ByVal value As DateTime)
-				privateFinishDate = value
-			End Set
-		End Property
+        Public Property StartDate As Date
 
-		Private privateIsComplete As Boolean
-		Public Property IsComplete() As Boolean
-			Get
-				Return privateIsComplete
-			End Get
-			Set(ByVal value As Boolean)
-				privateIsComplete = value
-			End Set
-		End Property
+        Public Property FinishDate As Date
 
-		Private privatePriority As Priority
-		Public Property Priority() As Priority
-			Get
-				Return privatePriority
-			End Get
-			Set(ByVal value As Priority)
-				privatePriority = value
-			End Set
-		End Property
-	End Class
+        Public Property IsComplete As Boolean
+
+        Public Property Priority As Priority
+    End Class
 End Namespace
